a3ca2a47c6e70611b5e02002f50fe438aba88ffb
Beytullah.Orbay <beytullahorbay38@gmail.com>
2023-01-06 13:04:55 +0100

    (nicht fertig) Icons in der GUI abbilden

--- src/view/Base.java
+++ src/view/Base.java
@@ -1,6 +1,7 @@
 package view;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Base extends JFrame{
     private JPanel panel1;
@@ -20,13 +21,53 @@ public class Base extends JFrame{
     private JCheckBox checkBox3;
     private JCheckBox checkBox4;
     private JTable table1;
+    private JLabel label_orc1;
+    private JLabel orc2;
+    private JLabel orc3;
+    private JLabel orc4;
+    private JLabel orc5;
+    private JLabel orc6;
+    private JLabel orc7;
+    private JLabel orc8;
+    private JLabel orc9;
+    private JPanel panel_orc1;
 
     public Base(String name) {
         setName(name);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setSize(900,600);
+        this.setSize(1300,600);
         //JButton button = new JButton("Click me");
+
+        //JPanel panel = new JPanel();
+        ImageIcon image = new ImageIcon("src/Icons/Orcs_Orcling.png");
+        JLabel label = new JLabel(image);
+        panel_orc1 = new JPanel(panel_orc1.getLayout());
+        //panel_orc1.add(label);
+        // Klasse aus Icons Branch einfügen
+        label.setPreferredSize(new Dimension(200,200));
+
+        MyFrame frame = new MyFrame();
+        frame.add(panel_orc1);
+
         this.getContentPane().add(panel1); // Adds Button to content pane of frame
+        this.getContentPane().add(panel_orc1, BorderLayout.WEST);
         this.setVisible(true);
+
+
+    }
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+
+    public static class MyFrame extends JFrame {
+        public MyFrame() {
+            setTitle("Aguk");
+            setSize(100,100);
+            setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+            JPanel panel = new JPanel();
+            add(panel);
+        }
     }
 }
