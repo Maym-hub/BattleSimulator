364d3bfa2625f805e61ebbc06d715fbd1f321f41
Beytullah.Orbay <beytullahorbay38@gmail.com>
2023-01-25 15:17:02 +0100

    (nicht fertig) ImagePanel Klasse eingefügt, Icons in der GUI an der korrekten Stelle abbilden

--- src/view/Base.java
+++ src/view/Base.java
@@ -2,6 +2,7 @@ package view;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.Collection;
 
 public class Base extends JFrame{
     private JPanel panel1;
@@ -32,28 +33,45 @@ public class Base extends JFrame{
     private JLabel orc9;
     private JPanel panel_orc1;
 
+
     public Base(String name) {
         setName(name);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setSize(1300,600);
+        this.setSize(1500,600);
         //JButton button = new JButton("Click me");
 
         //JPanel panel = new JPanel();
+        /*** Test Icon wird erstellt (muss entfernt werden) **/
         ImageIcon image = new ImageIcon("src/Icons/Orcs_Orcling.png");
         JLabel label = new JLabel(image);
-        panel_orc1 = new JPanel(panel_orc1.getLayout());
-        //panel_orc1.add(label);
-        // Klasse aus Icons Branch einfügen
-        label.setPreferredSize(new Dimension(200,200));
 
-        MyFrame frame = new MyFrame();
-        frame.add(panel_orc1);
+        panel_orc1 = new JPanel(/*panel_orc1.getLayout()*/);
+        panel_orc1.add(label);
 
-        this.getContentPane().add(panel1); // Adds Button to content pane of frame
-        this.getContentPane().add(panel_orc1, BorderLayout.WEST);
+
+        label.setPreferredSize(new Dimension(150,150));
+
+        /*** Wird MyFrame benötigt? ****/
+        //MyFrame frame = new MyFrame();
+        //frame.add(panel_orc1);
+
+        /**** (Test) BorderLayout um Tabelle & Bilder sehen zu ***/
+        this.getContentPane().add(panel1, BorderLayout.EAST); // Adds Button to content pane of frame
+        this.getContentPane().add(panel_orc1/*, BorderLayout.WEST*/);
         this.setVisible(true);
 
+        /** Klasse "ImagePanel" aus Icons Branch wurde eingefügt um Bilder korrekt abzubilden**/
+        ImagePanel imagePanel = new ImagePanel();
+        this.getContentPane().add(imagePanel, BorderLayout.WEST);
 
+        /**** Test das "Label" durch Bilder zu ersetzen ****/
+
+        panel_orc1.remove(label_orc1);
+        panel_orc1.add(imagePanel);
+        panel_orc1.validate();
+        panel_orc1.repaint();
+       // panel_orc1.setPreferredSize(new Dimension(50,50));
+        /****/
     }
 
     private void createUIComponents() {
