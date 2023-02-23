7392e29db12f98cd97bbb2d1352523e54f1c2acc
Beytullah.Orbay <beytullahorbay38@gmail.com>
2023-02-23 11:02:59 +0100

    ActionListener für OurTroups Buttons hinzugefügt, Textfelder gefärbt, Klasse "NumericDocumentFilter" hinzugefügt damit die Textfelder nur Zahlen haben und von 0 bis 100 gehen, Alle +1/+5 und All Buttons funktionieren

--- src/view/Base.java
+++ src/view/Base.java
@@ -1,6 +1,7 @@
 package view;
 
 import javax.swing.*;
+import javax.swing.text.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -16,11 +17,9 @@ public class Base extends JFrame{
     private JButton a5_Militia_Button;
     private JButton fill_Militia_Button;
     private JButton all_Militia_Button;
-    private JTextField textField2;
-    private JTextField textField3;
     private JButton clearEnemyTroupsButton;
-    private JCheckBox checkBox3;
-    private JCheckBox checkBox4;
+    private JCheckBox boss1CheckBox;
+    private JCheckBox boss2CheckBox;
     private JTable table1;
     private JLabel label_orc1;
     private JLabel orc2;
@@ -74,6 +73,15 @@ public class Base extends JFrame{
     private JTextField Cavalry_textField;
     private JTextField Cuirassier_textField;
     private JTextField Cannoneer_textField;
+    private JTextField Orkling_textField;
+    private JTextField OrcHunter_textField;
+    private JTextField OrcRaiders_textField;
+    private JTextField EliteOrcHunters_textField;
+    private JTextField OrcVeteran_textField;
+    private JTextField EliteOrcSniper_textField;
+    private JTextField WargRider_textField;
+    private JTextField OrcVanguard_textField;
+    private JTextField OrcDemolisher_textField;
 
 
     public Base(String name) {
@@ -116,11 +124,44 @@ public class Base extends JFrame{
        // panel_orc1.setPreferredSize(new Dimension(50,50));
         /****/
 
+        /*** Die Klasse "NumericDocumentFilter" wird hier verwendet damit nur Zahlen und von 0-100 in den Textfeldern eingegeben werden dürfen **/
+        ((AbstractDocument) Militia_textField.getDocument()).setDocumentFilter(new NumericDocumentFilter());
+        ((AbstractDocument) Archer_textField.getDocument()).setDocumentFilter(new NumericDocumentFilter());
+        ((AbstractDocument) Footsoldier_textField.getDocument()).setDocumentFilter(new NumericDocumentFilter());
+        ((AbstractDocument) LongBowArcher_textField.getDocument()).setDocumentFilter(new NumericDocumentFilter());
+        ((AbstractDocument) Knight_textField.getDocument()).setDocumentFilter(new NumericDocumentFilter());
+        ((AbstractDocument) Crossbowman_textField.getDocument()).setDocumentFilter(new NumericDocumentFilter());
+        ((AbstractDocument) Cavalry_textField.getDocument()).setDocumentFilter(new NumericDocumentFilter());
+        ((AbstractDocument) Cuirassier_textField.getDocument()).setDocumentFilter(new NumericDocumentFilter());
+        ((AbstractDocument) Cannoneer_textField.getDocument()).setDocumentFilter(new NumericDocumentFilter());
+
+
+        ((AbstractDocument) Orkling_textField.getDocument()).setDocumentFilter(new NumericDocumentFilter());
+        ((AbstractDocument) OrcHunter_textField.getDocument()).setDocumentFilter(new NumericDocumentFilter());
+        ((AbstractDocument) OrcRaiders_textField.getDocument()).setDocumentFilter(new NumericDocumentFilter());
+        ((AbstractDocument) EliteOrcHunters_textField.getDocument()).setDocumentFilter(new NumericDocumentFilter());
+        ((AbstractDocument) OrcVeteran_textField.getDocument()).setDocumentFilter(new NumericDocumentFilter());
+        ((AbstractDocument) EliteOrcSniper_textField.getDocument()).setDocumentFilter(new NumericDocumentFilter());
+        ((AbstractDocument) WargRider_textField.getDocument()).setDocumentFilter(new NumericDocumentFilter());
+        ((AbstractDocument) OrcVanguard_textField.getDocument()).setDocumentFilter(new NumericDocumentFilter());
+        ((AbstractDocument) OrcDemolisher_textField.getDocument()).setDocumentFilter(new NumericDocumentFilter());
+
+        /********/
+
+
         /*** Die ganzen ActionListeners auf alle Buttons ***/
         clearOurTroupsButton1.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                Militia_textField.setText(String.valueOf(0));
+                Archer_textField.setText(String.valueOf(0));
+                Footsoldier_textField.setText(String.valueOf(0));
+                LongBowArcher_textField.setText(String.valueOf(0));
+                Knight_textField.setText(String.valueOf(0));
+                Crossbowman_textField.setText(String.valueOf(0));
+                Cavalry_textField.setText(String.valueOf(0));
+                Cuirassier_textField.setText(String.valueOf(0));
+                Cannoneer_textField.setText(String.valueOf(0));
             }
         });
         a1_Militia_Button.addActionListener(new ActionListener() {
@@ -149,7 +190,9 @@ public class Base extends JFrame{
         all_Militia_Button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                int currentValue = Integer.parseInt(Militia_textField.getText() );
+                if(currentValue < 100)
+                    Militia_textField.setText(String.valueOf(100));
             }
         });
         a1_Archer_Button.addActionListener(new ActionListener() {
@@ -177,19 +220,25 @@ public class Base extends JFrame{
         all_Archer_Button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                int currentValue = Integer.parseInt(Archer_textField.getText() );
+                if(currentValue < 100)
+                    Archer_textField.setText(String.valueOf(100));
             }
         });
         a1_Footsoldier_Button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                int currentValue = Integer.parseInt(Footsoldier_textField.getText());
+                if(currentValue < 100)
+                    Footsoldier_textField.setText(Integer.toString(Integer.parseInt(Footsoldier_textField.getText()) + 1));
             }
         });
         a5_Footsoldier_Button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                int currentValue = Integer.parseInt(Footsoldier_textField.getText());
+                if(currentValue < 100)
+                    Footsoldier_textField.setText(Integer.toString(Integer.parseInt(Footsoldier_textField.getText()) + 5));
             }
         });
         fill_Footsoldier_Button.addActionListener(new ActionListener() {
@@ -201,19 +250,25 @@ public class Base extends JFrame{
         all_Footsoldier_Button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                int currentValue = Integer.parseInt(Footsoldier_textField.getText() );
+                if(currentValue < 100)
+                    Footsoldier_textField.setText(String.valueOf(100));
             }
         });
         a1_LongBowArcher_Button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                int currentValue = Integer.parseInt(LongBowArcher_textField.getText());
+                if(currentValue < 100)
+                    LongBowArcher_textField.setText(Integer.toString(Integer.parseInt(LongBowArcher_textField.getText()) + 1));
             }
         });
         a5_LongBowArcher_Button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                int currentValue = Integer.parseInt(LongBowArcher_textField.getText());
+                if(currentValue < 100)
+                    LongBowArcher_textField.setText(Integer.toString(Integer.parseInt(LongBowArcher_textField.getText()) + 5));
             }
         });
         fill_LongBowArcher_Button.addActionListener(new ActionListener() {
@@ -225,19 +280,25 @@ public class Base extends JFrame{
         all_LongBowArcher_Button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                int currentValue = Integer.parseInt(LongBowArcher_textField.getText() );
+                if(currentValue < 100)
+                    LongBowArcher_textField.setText(String.valueOf(100));
             }
         });
         a1_Knight_Button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                int currentValue = Integer.parseInt(Knight_textField.getText());
+                if(currentValue < 100)
+                    Knight_textField.setText(Integer.toString(Integer.parseInt(Knight_textField.getText()) + 1));
             }
         });
         a5_Knight_Button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                int currentValue = Integer.parseInt(Knight_textField.getText());
+                if(currentValue < 100)
+                    Knight_textField.setText(Integer.toString(Integer.parseInt(Knight_textField.getText()) + 5));
             }
         });
         fill_Knight_Button.addActionListener(new ActionListener() {
@@ -249,19 +310,25 @@ public class Base extends JFrame{
         all_Knight_Button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                int currentValue = Integer.parseInt(Knight_textField.getText() );
+                if(currentValue < 100)
+                    Knight_textField.setText(String.valueOf(100));
             }
         });
         a1_Crossbowman_Button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                int currentValue = Integer.parseInt(Crossbowman_textField.getText());
+                if(currentValue < 100)
+                    Crossbowman_textField.setText(Integer.toString(Integer.parseInt(Crossbowman_textField.getText()) + 1));
             }
         });
         a5_Crossbowman_Button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                int currentValue = Integer.parseInt(Crossbowman_textField.getText());
+                if(currentValue < 100)
+                    Crossbowman_textField.setText(Integer.toString(Integer.parseInt(Crossbowman_textField.getText()) + 5));
             }
         });
         fill_Crossbowman_Button.addActionListener(new ActionListener() {
@@ -273,19 +340,25 @@ public class Base extends JFrame{
         all_Crossbowman_Button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                int currentValue = Integer.parseInt(Crossbowman_textField.getText() );
+                if(currentValue < 100)
+                    Crossbowman_textField.setText(String.valueOf(100));
             }
         });
         a1_Cavalry_Button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                int currentValue = Integer.parseInt(Cavalry_textField.getText());
+                if(currentValue < 100)
+                    Cavalry_textField.setText(Integer.toString(Integer.parseInt(Cavalry_textField.getText()) + 1));
             }
         });
         a5_Cavalry_Button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                int currentValue = Integer.parseInt(Cavalry_textField.getText());
+                if(currentValue < 100)
+                    Cavalry_textField.setText(Integer.toString(Integer.parseInt(Cavalry_textField.getText()) + 5));
             }
         });
         fill_Cavalry_Button.addActionListener(new ActionListener() {
@@ -297,19 +370,25 @@ public class Base extends JFrame{
         all_Cavalry_Button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                int currentValue = Integer.parseInt(Cavalry_textField.getText() );
+                if(currentValue < 100)
+                    Cavalry_textField.setText(String.valueOf(100));
             }
         });
         a1_Cuirassier_Button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                int currentValue = Integer.parseInt(Cuirassier_textField.getText());
+                if(currentValue < 100)
+                    Cuirassier_textField.setText(Integer.toString(Integer.parseInt(Cuirassier_textField.getText()) + 1));
             }
         });
         a5_Cuirassier_Button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                int currentValue = Integer.parseInt(Cuirassier_textField.getText());
+                if(currentValue < 100)
+                    Cuirassier_textField.setText(Integer.toString(Integer.parseInt(Cuirassier_textField.getText()) + 5));
             }
         });
         fill_Cuirassier_Button.addActionListener(new ActionListener() {
@@ -321,19 +400,25 @@ public class Base extends JFrame{
         all_Cuirassier_Button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                int currentValue = Integer.parseInt(Cuirassier_textField.getText() );
+                if(currentValue < 100)
+                    Cuirassier_textField.setText(String.valueOf(100));
             }
         });
         a1_Cannoneer_Button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                int currentValue = Integer.parseInt(Cannoneer_textField.getText());
+                if(currentValue < 100)
+                    Cannoneer_textField.setText(Integer.toString(Integer.parseInt(Cannoneer_textField.getText()) + 1));
             }
         });
         a5_Cannoneer_Button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                int currentValue = Integer.parseInt(Cannoneer_textField.getText());
+                if(currentValue < 100)
+                    Cannoneer_textField.setText(Integer.toString(Integer.parseInt(Cannoneer_textField.getText()) + 5));
             }
         });
         fill_Cannoneer_Button.addActionListener(new ActionListener() {
@@ -345,13 +430,23 @@ public class Base extends JFrame{
         all_Cannoneer_Button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                int currentValue = Integer.parseInt(Cannoneer_textField.getText() );
+                if(currentValue < 100)
+                    Cannoneer_textField.setText(String.valueOf(100));
             }
         });
         clearEnemyTroupsButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                Orkling_textField.setText(String.valueOf(0));
+                OrcHunter_textField.setText(String.valueOf(0));
+                OrcRaiders_textField.setText(String.valueOf(0));
+                EliteOrcHunters_textField.setText(String.valueOf(0));
+                OrcVeteran_textField.setText(String.valueOf(0));
+                EliteOrcSniper_textField.setText(String.valueOf(0));
+                WargRider_textField.setText(String.valueOf(0));
+                OrcVanguard_textField.setText(String.valueOf(0));
+                OrcDemolisher_textField.setText(String.valueOf(0));
             }
         });
     }
@@ -370,4 +465,37 @@ public class Base extends JFrame{
             add(panel);
         }
     }
+    static class NumericDocumentFilter extends DocumentFilter {
+        @Override
+        public void insertString(FilterBypass fb, int offset, String string, AttributeSet attr)
+                throws BadLocationException {
+            StringBuilder sb = new StringBuilder();
+            sb.append(fb.getDocument().getText(0, fb.getDocument().getLength()));
+            sb.insert(offset, string);
+            if (test(sb.toString())) {
+                super.insertString(fb, offset, string, attr);
+            }
+        }
+
+        @Override
+        public void replace(FilterBypass fb, int offset, int length, String string, AttributeSet attr)
+                throws BadLocationException {
+            StringBuilder sb = new StringBuilder();
+            sb.append(fb.getDocument().getText(0, fb.getDocument().getLength()));
+            sb.replace(offset, offset + length, string);
+            if (test(sb.toString())) {
+                super.replace(fb, offset, length, string, attr);
+            }
+        }
+        private boolean test(String text) {
+            try {
+                int value = Integer.parseInt(text);
+                return (value >= 0 && value <= 100);
+            } catch (NumberFormatException e) {
+                return false;
+            }
+        }
+    }
+
+
 }
